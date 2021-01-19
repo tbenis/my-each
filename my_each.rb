262ec2e1b6d9e0e8cacb9b4d8ea8885c045b105e
Benis Tambe <will.jenifer@yahoo.com>
2021-01-19 11:09:40 -0500

    commit tree a7808fad27d391e0de57442ad3c9bf026eb4c87d

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,26 @@
-def my_each # put argument(s) here
-  # code here
+# def my_each(array) # put argument(s) here
+#   i = 0
+#   # code here
+#   while i < array.length
+#     yield array[i]
+#     i+=1
+#   end
+#   array
+# end
+
+# collection = [1, 2, 3, 4]
+# my_each(collection) do |i|
+#    i
+# end 
+
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  array
+end
+my_each(["1", "2", "3", "4"]) do |i|
+  "#{i} is my favorite number!"
 end
